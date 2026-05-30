ffcd19b7f771810e866b007a04ba2303465ea644
jrcontreras24-dot <jr.contreras24@info.uas.edu.mx>
2026-05-29 21:22:14 -0700

    Update database import path in pelicula model

--- pelicula.model.js
+++ pelicula.model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('./database');
 
 const Pelicula = sequelize.define('Pelicula', {
   titulo: {
